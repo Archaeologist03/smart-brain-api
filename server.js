3b8b190bb35f121b1534b6d99555be45f3e3ae8c
Archaeologist03 <omniiknight@hotmail.com>
2019-03-10 21:48:49 +0100

    Registering user. (make backend work with front end when user users post /register.

--- server.js
+++ server.js
@@ -4,17 +4,21 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const postgres = knex({
+const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
     user: 'archaeologist',
-    password: 'bandera123', // or bandera..
+    password: 'bandera',
     database: 'smart-brain-db',
   },
 });
 
-console.log(postgres.select('*').from('users'));
+db.select('*')
+  .from('users')
+  .then(data => {
+    console.log(data);
+  });
 
 const app = express();
 
@@ -83,17 +87,17 @@ app.post('/signin', (req, res) => {
 //REGISTER
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  //   bcrypt.hash(password, null, null, function(err, hash) {
-  //     console.log(hash);
-  // });
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+  db('users')
+    .returning('*')
+    .insert({
+      email,
+      name,
+      joined: new Date(),
+    })
+    .then(user => {
+      res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('Unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
