2d54c2eceec5fb62dcc22652042e8095c3c27f55
Archaeologist03 <omniiknight@hotmail.com>
2019-03-12 00:06:22 +0100

    Updating database. (host to heroku pq db).

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-spherical-33492',
     user: 'archaeologist',
     password: 'bandera',
     database: 'smart-brain-db',
