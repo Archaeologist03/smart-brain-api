9182a697ca42dfd135b9a3683dec2d7117cb47c1
Archaeologist03 <omniiknight@hotmail.com>
2019-06-21 23:16:26 +0200

    Updating profile, Jwt-auth.

--- controllers/profile.js
+++ controllers/profile.js
@@ -15,6 +15,25 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('Error getting user'));
 };
 
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params;
+  const { name, age, pet } = req.body.formInput;
+  db('users')
+    .where({ id })
+    .update({ name: name })
+    .then(resp => {
+      if (resp) {
+        console.log(name, 12121);
+
+        res.json('success');
+      } else {
+        res.status(400).json('Unable to update');
+      }
+    })
+    .catch(err => res.status(400).json('error updating user'));
+};
+
 module.exports = {
   handleProfileGet,
+  handleProfileUpdate,
 };
